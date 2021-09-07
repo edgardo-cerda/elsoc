31933d636ea261d081969fd61464825c9106519b
edgardo-cerda <edgardo.cerda.e@gmail.com>
2021-09-07 16:53:11 -0300

    Se agrega datos de ejemplo elsoc_example

---
+++ R/elsoc_example.R
@@ -0,0 +1,9 @@
+#' Subset of ELSOC 2016-2021 for running examples
+#'
+#' @format A data frame with 1525 rows and 8 variables:
+#' \describe{
+#'   \item{idencuesta}{casewise id}
+#'   \item{ola}{wave of the study}
+#'   ...
+#' }
+"elsoc_example"
