9de60d6b1638c1a66a84ff7eb7bc3200dbb46d41
edgardo-cerda <edgardo.cerda.e@gmail.com>
2021-09-14 14:09:17 -0300

    se agrega función lookfor y data frame de listado de variables en que se busca

---
+++ R/lookfor.R
@@ -0,0 +1,20 @@
+#' lookfor
+#'
+#' Function to find variable names, general concept and label based on a pattern in the ELSOC variable list. Regular expressions for the pattern is supported
+#' @param pattern Character string to be looked for in the ELSOC variable list
+#'
+#' @export
+#'
+#' @examples
+#'
+#' lookfor('c01')
+#' lookfor('cohesion')
+lookfor <- function(pattern = '.') {
+
+        pos1 <- which(grepl(pattern = pattern, x = tolower(elsoc::variable_list$variable)))
+        pos2 <- which(grepl(pattern = pattern, x = tolower(elsoc::variable_list$label)))
+        pos3 <- which(grepl(pattern = pattern, x = tolower(elsoc::variable_list$general_concept)))
+
+        pos <- unique(c(pos1, pos2, pos3))
+        return(elsoc::variable_list[pos, c('variable', 'general_concept', 'label')])
+}
---
+++ R/variable_list.R
@@ -0,0 +1,6 @@
+#' Variable list of ELSOC databases
+#'
+#' @format A data frame with 720 rows and 5 variables
+#'
+#' @source variable_list
+"variable_list"
