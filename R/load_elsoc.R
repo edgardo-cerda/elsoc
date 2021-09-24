cf0f0c65dce883e980f7b492754f16b71cfeec0b
edgardo-cerda <edgardo.cerda.e@gmail.com>
2021-09-24 14:14:05 -0300

    correccion de url de datos

--- R/load_elsoc.R
+++ R/load_elsoc.R
@@ -18,12 +18,11 @@ load_elsoc <- function(url = NULL, format = 'long') {
     #     url <- "https://dataverse.harvard.edu/api/access/datafile/4606527"
     # }
 
-
     if (format == 'long') {
-        url <- 'https://www.dropbox.com/s/6guepna1ij5sluh/ELSOC_Long_2016_2021_beta_R.RData?dl=1'
+        url <- 'https://www.dropbox.com/s/mi82mspp03qrqm0/ELSOC_Long_2016_2021_beta_R.RData?dl=1'
         message("Loading beta version of ELSOC 2016-2021 in Long format")
     } else {
-        url <- 'https://www.dropbox.com/s/hv5hmfg36fqcglg/ELSOC_Wide_2016_2021_beta_R.RData?dl=1'
+        url <- 'https://www.dropbox.com/s/r1cvseafsr8vj22/ELSOC_Wide_2016_2021_beta_R.RData?dl=1'
         message("Loading beta version of ELSOC 2016-2021 in Wide format")
     }
 
