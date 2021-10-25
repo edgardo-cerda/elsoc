e74c0de4422568fc839f83756eba8cf855a595a4
edgardo-cerda <edgardo.cerda.e@gmail.com>
2021-10-25 11:22:11 -0300

    Se agrega función is_nsnr

---
+++ R/is_nsnr.R
@@ -0,0 +1,18 @@
+
+#' is_nsnr
+#'
+#' Function to test if multiple variables contain the values associated with Doesn't know or Doesn't answer (default = c(-888, -999)).
+#' Useful for testing multiple variables in shorter code
+#'
+#' @param ... variables to test
+#' @param values Values to test if are contained in variables
+#'
+#' @export
+#'
+#' @examples
+#' elsoc_example %>% dplyr::filter(have.value(c01, value = c(-888, -999)))
+#'
+is_nsnr <- function(..., values = c(-888, -999)){
+    purrr::reduce(purrr::map(.x = list(...), .f = ~{. %in% values}), `|`)
+}
+
