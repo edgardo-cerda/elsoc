3b94772bbd25adf57131c7c9a838c39694f48de1
edgardo-cerda <edgardo.cerda.e@gmail.com>
2021-11-05 12:27:57 -0300

    Cambio de nombre de función values a values_elsoc

--- R/values_elsoc.R
+++ R/values_elsoc.R
@@ -1,4 +1,4 @@
-#' values
+#' values_elsoc
 #'
 #' Function to return variable values and value labels
 #'
@@ -6,11 +6,13 @@
 #'
 #' @export
 #'
-values <- function(x) {
+values_elsoc <- function(x) {
 
     values <- data.frame(strsplit(elsoc::variable_list$codigos_respuesta[elsoc::variable_list$variable == x], '\\|'),
                          strsplit(elsoc::variable_list$opciones_respuesta[elsoc::variable_list$variable == x], '\\|'))
     names(values) <- c('values', 'value_labels')
+
     return(values)
+
 }
 
