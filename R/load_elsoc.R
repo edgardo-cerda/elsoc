76205a89c1a1031d25a0831f8072a765799d1a1e
edgardo-cerda <edgardo.cerda.e@gmail.com>
2021-12-03 13:09:40 -0300

    Corrección de error con argumento name

--- R/load_elsoc.R
+++ R/load_elsoc.R
@@ -23,50 +23,49 @@
 load_elsoc <- function(data = 'long', name = NULL) {
 
     if (data == 'long') {
-        if (is.null(name)) assign_name <- 'elsoc_long_2016_2021'
+        if (is.null(name)) name <- 'elsoc_long_2016_2021'
         message("Loading beta version of ELSOC 2016-2021 in Long format")
         load(url('https://www.dropbox.com/s/i3y7yoljhna7jtq/ELSOC_Long_2016_2021_v1.00_R.RData?dl=1'))
         dataobj <- elsoc_long_2016_2021
     }
 
     else if (data == 'wide') {
-        if (is.null(name)) assign_name <- 'elsoc_wide_2016_2021'
+        if (is.null(name)) name <- 'elsoc_wide_2016_2021'
         message("Loading beta version of ELSOC 2016-2021 in Wide format")
         load(url('https://www.dropbox.com/s/zbr2ls05wu3fimc/ELSOC_Wide_2016_2021_v1.00_R.RData?dl=1'))
         dataobj <- elsoc_wide_2016_2021
     }
 
     else if (data == 'territorial-2016') {
-        if (is.null(name)) assign_name <- 'elsoc_terr_2016'
+        if (is.null(name)) name <- 'elsoc_terr_2016'
         dataobj <- read.table(url('https://dataverse.harvard.edu/api/access/datafile/5216817'), sep = '\t',
                                   header = TRUE, fileEncoding = "UTF-8")
     }
 
     else if (data == 'territorial-2017') {
-        if (is.null(name)) assign_name <- 'elsoc_terr_2017'
+        if (is.null(name)) name <- 'elsoc_terr_2017'
         dataobj <- read.table(url('https://dataverse.harvard.edu/api/access/datafile/5216836'), sep = '\t',
                                   header = TRUE, fileEncoding = "UTF-8")
     }
 
     else if (data == 'territorial-2018') {
-        if (is.null(name)) assign_name <- 'elsoc_terr_2018'
+        if (is.null(name)) name <- 'elsoc_terr_2018'
         dataobj <- read.table(url('https://dataverse.harvard.edu/api/access/datafile/5216941'), sep = '\t',
                                   header = TRUE, fileEncoding = "UTF-8")
     }
 
     else if (data == 'territorial-2019') {
-        if (is.null(name)) assign_name <- 'elsoc_terr_2019'
+        if (is.null(name)) name <- 'elsoc_terr_2019'
         dataobj <- read.table(url('https://dataverse.harvard.edu/api/access/datafile/5216990'), sep = '\t',
                                   header = TRUE, fileEncoding = "UTF-8")
     }
 
     else if (data == 'elsoc-cit-2016') {
-        if (is.null(name)) assign_name <- 'elsoc_cit_2016'
+        if (is.null(name)) name <- 'elsoc_cit_2016'
         dataobj <- read.table(url('https://dataverse.harvard.edu/api/access/datafile/4892173'), sep = '\t',
                                   header = TRUE, fileEncoding = "UTF-8")
     }
 
-    assign(assign_name, dataobj, envir = .GlobalEnv)
+    assign(name, dataobj, envir = .GlobalEnv)
 
 }
-
